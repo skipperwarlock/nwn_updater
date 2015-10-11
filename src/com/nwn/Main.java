b443c4b6b813d743e35edf59c54114795dc32f18
skipperwarlock <samcantrell@yahoo.com>
2015-10-11 05:23:25 -0400

    Updater no longer singleton to support updating multiple servers at once. Some threading logic added

--- src/com/nwn/Main.java
+++ src/com/nwn/Main.java
@@ -9,10 +9,19 @@ public class Main {
     public static void testing(){
         FileHandler.downloadFile("http://supergsego.com/apache//commons/beanutils/binaries/commons-beanutils-1.9.2-bin.tar.gz", "C:\\Users\\samca\\Downloads\\commons-beanutils-1.9.2-bin.tar.gz");
         Path test = Paths.get("C:\\Users\\samca\\Downloads\\commons-beanutils-1.9.2-bin.tar.gz");
-        System.out.println(FileHandler.getFileExtension(test));
+        System.out.println(FileHandler.getFileExtension(test.getFileName().toString()));
+    }
+
+    public static void updaterThreadExample(){
+        NwnUpdater nwnUpdater = new NwnUpdater();
+        nwnUpdater.setNwnRootPath(null);
+        nwnUpdater.setServerFileList(null);
+
+        Thread updateThread = new Thread(nwnUpdater, "Update Thread");
+        updateThread.start();
     }
 
     public static void main(String[] args) {
-
+        ;
     }
 }
--- src/com/nwn/NwnUpdater.java
+++ src/com/nwn/NwnUpdater.java
@@ -6,16 +6,36 @@ import java.util.ArrayList;
 /**
  * Created by Sam on 10/10/2015.
  */
-public class NwnUpdater {
+public class NwnUpdater implements Runnable{
     private ArrayList<ServerFile> serverFileList;
     private Path nwnRootPath;
-    private static NwnUpdater instance = new NwnUpdater();
 
-    public synchronized static NwnUpdater getInstance() {
-        return instance;
+    public NwnUpdater() {
     }
 
-    private NwnUpdater(Path serverFile, Path nwnRoot) {
+    @Override
+    public void run() {
+        //by this point, serverFileList should already be populated
+        //we need to:
+        //parse the necessary directories to determine if all files are present
+        //download files not present
+        //move files to correct directories
+        //delete any tmp data
+    }
 
+    public ArrayList<ServerFile> getServerFileList(){
+        return serverFileList;
+    }
+
+    public Path getNwnRootPath(){
+        return nwnRootPath;
+    }
+
+    public void setServerFileList(ArrayList<ServerFile> newServerFileList){
+        serverFileList = newServerFileList;
+    }
+
+    public void setNwnRootPath(Path newNwnRootPath){
+        nwnRootPath = newNwnRootPath;
     }
 }
